c1d118bb9857d9ad1bdaadd46c59f6600f6e27c4
trankillity <trankillity@gmail.com>
2021-03-07 11:59:20 +1000

    Update

--- keymap.c
+++ keymap.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-// Notes
+// __attribute__((weak)) bool process_record_keymap(uint16_t keycode, keyrecord_t *record) { return true; }
 
 // Declare layers
 enum custom_layers {
@@ -16,7 +16,10 @@ enum custom_keycodes {
     QWERTY = SAFE_RANGE,
     COLEMAK,
     LT_EDT,
-    LT_NUM
+    LT_NUM,
+    CW_TOG,
+    CW_ENA,
+    CW_DIS
 };
 
 // Define GASC shortcuts
@@ -41,26 +44,11 @@ enum custom_keycodes {
 
 // Define editing layer shortcuts
 // Format: EL_Mod_Key
-#define EL_A_UP LALT(KC_UP)
-#define EL_A_DWN LALT(KC_DOWN)
-#define EL_C_BCK LCTL(KC_BSPC)
-#define EL_C_DEL LCTL(KC_DEL)
-#define EL_C_HME LCTL(KC_HOME)
-#define EL_C_LFT LCTL(KC_LEFT)
-#define EL_C_RGT LCTL(KC_RGHT)
-#define EL_C_END LCTL(KC_END)
-#define EL_S_HME LSFT(KC_HOME)
-#define EL_S_END LSFT(KC_END)
-#define EL_CS_HME LCTL(LSFT(KC_HOME))
-#define EL_CS_END LCTL(LSFT(KC_END))
 #define EL_C_Z LCTL(KC_Z)
 #define EL_C_X LCTL(KC_X)
 #define EL_C_C LCTL(KC_C)
 #define EL_C_V LCTL(KC_V)
-#define EL_CS_V LCTL(LSFT(KC_V))
-#define EL_W_LFT C(G(KC_LEFT))
-#define EL_W_RGT C(G(KC_RGHT))
-#define EL_W_MAN G(KC_TAB)
+#define EL_C_Y LCTL(KC_Y)
 
 #define LT_EDT LT(_EDIT,KC_ENT)
 #define LT_NUM LT(_NUM,KC_ENT)
@@ -81,7 +69,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [BR_SQR] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC),           // [,]
     [BR_CRL] = ACTION_TAP_DANCE_DOUBLE(S(KC_LBRC), S(KC_RBRC)),     // {,}
     [BR_RND] = ACTION_TAP_DANCE_DOUBLE(S(KC_9), S(KC_0)),           // (,)
-    [KL_SFT] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),           // Shift,CAPS
+    [KL_SFT] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, CW_ENA),            // Shift,CAPS
     [TG_GME] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_ENT, _GAME)         // Enter,_GAME
 };
 
@@ -103,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MPLY,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_DEL,
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC,
         KC_GESC,    HR_G_L,     HR_Q_A_L,   HR_Q_S_L,   HR_Q_C_L,   KC_G,       KC_H,       HR_Q_C_R,   HR_Q_S_R,   HR_Q_A_R,   HR_Q_G_R,   KC_QUOT,
-        KC_BSLS,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    TD(TG_GME),
+        KC_BSLS,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_CAPS,
         KC_LGUI,    KC_LALT,    TD(KL_SFT), LCTL_T(KC_EQL),  LT_EDT,           KC_SPC,           LT_NUM,     LCTL_T(KC_MINS),  KC_LSFT,    KC_LALT,    KC_LGUI
         ),
 /* Colemak-DH layer:
@@ -123,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MPLY,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_DEL,
         KC_TAB,     KC_Q,       KC_W,       KC_F,       KC_P,       KC_B,       KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCLN,    KC_BSPC,
         KC_GESC,    HR_G_L,     HR_C_A_L,   HR_C_S_L,   HR_C_C_L,   KC_G,       KC_M,       HR_C_C_R,   HR_C_S_R,   HR_C_A_R,   HR_C_G_R,   KC_QUOT,
-        KC_BSLS,    KC_Z,       KC_X,       KC_C,       KC_D,       KC_V,       KC_K,       KC_H,       KC_COMM,    KC_DOT,     KC_SLSH,    TD(TG_GME),
+        KC_BSLS,    KC_Z,       KC_X,       KC_C,       KC_D,       KC_V,       KC_K,       KC_H,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_CAPS,
         KC_LGUI,    KC_LALT,    TD(KL_SFT), LCTL_T(KC_EQL),  LT_EDT,           KC_SPC,           LT_NUM,     LCTL_T(KC_MINS),  KC_LSFT,    KC_LALT,    KC_LGUI
         ),
 /* Gaming layer:
@@ -140,33 +128,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
     [_GAME] = LAYOUT_preonic_1x2uC(
-        KC_TRNS,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_DEL,
-        KC_TRNS,    KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC,
-        KC_TRNS,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
-        KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_TRNS,
-        KC_NO,      KC_TRNS,    KC_LSFT,    KC_TRNS,    KC_TRNS,          KC_TRNS,          KC_TRNS,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT
+        _______,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_DEL,
+        _______,    KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC,
+        _______,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
+        KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    _______,
+        XXXXXXX,      _______,    KC_LSFT,    _______,    _______,          _______,          _______,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT
         ),
 /* Editing layer:
  * ,-----------------------------------------------------------------------------------.
  * |Rotary|      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |CS+Hme| C+Bck| A+Up | C+Del|CS+End| C+Hme| C+Lft|  Up  | C+Rgt| C+End|      |
+ * | Tab  |      |      |      |      |      | PgUp | BkSpc|  Up  |  Del | PgDn |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc~ | S+Hme|  Bck |A+Down|  Del | S+End| Home | Left | Down | Right|  End |      |
+ * | Esc~ |      |      |      |      |      | Home | Left | Down | Right|  End |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  C+Z |  C+C |  C+X |  C+V | CS+V | PgUp | <  > | [  ] | {  } | PgDn |      |
+ * |      |  C+Z |  C+C |  C+X |  C+V |  C+Y |      | <  > | [  ] | {  } |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |      |(hold)|             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
     [_EDIT] = LAYOUT_preonic_1x2uC(
-        KC_TRNS,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
-        KC_TRNS,    EL_CS_HME,  EL_C_BCK,   EL_A_UP,    EL_C_DEL,   EL_CS_END,  EL_C_HME,   EL_C_LFT,   KC_UP,      EL_C_RGT,   EL_C_END,   KC_NO,
-        KC_TRNS,    EL_S_HME,   KC_BSPC,    EL_A_DWN,   KC_DEL,     EL_S_END,   KC_HOME,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_END,     KC_NO,
-        KC_TRNS,    EL_C_Z,     EL_C_X,     EL_C_C,     EL_C_V,     EL_CS_V,    KC_PGUP,    TD(BR_PNT), TD(BR_SQR), TD(BR_CRL), KC_PGDN,    KC_NO,
-        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,          KC_TRNS,          KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS
+        _______,    XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
+        _______,    _______,    _______,    _______,    _______,    _______,    KC_PGUP,    KC_BSPC,    KC_UP,      KC_DEL,     KC_PGDN,    _______,
+        _______,    _______,    _______,    _______,    _______,    _______,    KC_HOME,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_END,     _______,
+        _______,    EL_C_Z,    EL_C_X,     EL_C_C,    EL_C_V,    EL_C_Y,    XXXXXXX,      TD(BR_PNT), TD(BR_SQR), TD(BR_CRL), XXXXXXX,      XXXXXXX,
+        _______,    _______,    _______,    _______,    _______,          _______,          _______,    _______,    _______,    _______,    _______
         ),
-/* Numpad/Function layer:
+/* Numpad layer:
  * ,-----------------------------------------------------------------------------------.
  * |Rotary|      |      |      |      |      | NumLk|   $  |   %  |   ^  |   -  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -179,32 +167,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |(hold)|   0  |   .  | Enter|      |
  * `-----------------------------------------------------------------------------------'
  */
+
     [_NUM] = LAYOUT_preonic_1x2uC(
-        KC_TRNS,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NLCK,    KC_DLR,     KC_PERC,    KC_CIRC,    KC_PMNS,    KC_NO,
-        KC_TRNS,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_NO,      KC_PSCR,    KC_P7,      KC_P8,      KC_P9,      KC_PPLS,    KC_NO,
-        KC_TRNS,    KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_NO,      KC_INS,     KC_P4,      KC_P5,      KC_P6,      KC_PAST,    KC_NO,
-        KC_NO,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_NO,      KC_EQL,     KC_P1,      KC_P2,      KC_P3,      KC_PSLS,    KC_NO,
-        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,          KC_TRNS,          KC_TRNS,    KC_P0,      KC_PDOT,    KC_PENT,    KC_NO
+        _______,    KC_CIRC,      KC_NLCK,      KC_PSLS,      KC_PAST,      XXXXXXX,      XXXXXXX,    XXXXXXX,     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+        _______,    KC_PERC,      KC_P7,      KC_P8,      KC_P9,      KC_PMNS,      _______,    _______,      _______,      _______,      _______,    XXXXXXX,
+        _______,    KC_DLR,      KC_P4,      KC_P5,      KC_P6,      KC_PSLS,      _______,     _______,      _______,      _______,      _______,    XXXXXXX,
+        XXXXXXX,    TD(BR_RND),      KC_P1,      KC_P2,      KC_P3,     KC_PEQL,      _______,     _______,      _______,      _______,      _______,    XXXXXXX,
+        XXXXXXX,    XXXXXXX,    KC_P0,    KC_PDOT,    _______,          _______,          _______,    _______,      _______,    _______,    XXXXXXX
         ),
 /* Sys layer:
  * ,-----------------------------------------------------------------------------------.
- * |Rotary|RGBTog|      |      |      |      |      |      |      |      |      | Reset|
+ * |Rotary|      |      |      |      |      |      |      |      |      |      | Reset|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |      |      |      |      |      |      |DskLft| G+Up |DskRgt|      |      |
+ * | Tab  |      |      |      |  F9  |      |      |RGBTog| HUE+ | SAT+ | VAL+ |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc~ |      |      |      |      |      |      |G+Left|G+Down|G+Rght|      |      |
+ * | Esc~ |      |      |      |  F5  |      |      |RGBMDE| HUE- | SAT- | VAL- |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |QWERTY|Clemak|      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |  F3  |      |      |QWERTY|ColeDH|      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
     [_SYS] = LAYOUT_preonic_1x2uC(
-        KC_TRNS,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      RESET,
-        KC_TRNS,    KC_NO,  KC_NO,   KC_NO,    KC_NO,   KC_NO,  KC_NO,   LCTL(LGUI(KC_LEFT)),   LGUI(KC_UP),      LCTL(LGUI(KC_RIGHT)),   KC_NO,   KC_NO,
-        KC_TRNS,    KC_NO,   KC_NO,    KC_NO,   KC_NO,     KC_NO,   KC_NO,    LGUI(KC_LEFT),    LGUI(KC_DOWN),    LGUI(KC_RGHT),    KC_END,     KC_NO,
-        KC_TRNS,    QWERTY,     COLEMAK,     KC_NO,     KC_NO,     KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,
-        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_TRNS,          KC_TRNS,          KC_TRNS,    KC_NO,      KC_NO,      KC_NO,      KC_NO
+        _______,    XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      RESET,
+        _______,    KC_F10,    KC_F7,       KC_F8,      KC_F9,      XXXXXXX,      XXXXXXX,      RGB_TOG,  RGB_HUI,   RGB_SAI,    RGB_VAI,       XXXXXXX,
+        _______,    KC_F11,    KC_F4,       KC_F5,      KC_F6,      KC_PSCR,      XXXXXXX,      RGB_MOD,   RGB_HUD,    RGB_SAD,   RGB_VAD, XXXXXXX,
+        _______,    KC_F12,    KC_F1,       KC_F2,      KC_F3,      KC_INS,      XXXXXXX,      QWERTY,     COLEMAK,     TG(_GAME),     XXXXXXX,      XXXXXXX,
+        KC_APP,      XXXXXXX,      XXXXXXX,      XXXXXXX,      _______,          _______,          _______,    XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
         )
 };
 
@@ -257,42 +246,71 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 //     }
 // }
 
+#ifdef CAPSWORD_ENABLE
+// Caps word: deactivate caps lock following a word
+void process_caps_word(uint16_t keycode, keyrecord_t *record) {
+	// Get the base key code of a mod or layer tap
+	switch (keycode) {
+	case QK_MOD_TAP ... QK_MOD_TAP_MAX:
+	case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
+		if (!record->tap.count) { return; }
+		keycode = keycode & 0xFF;
+	}
+	// Toggle caps lock with the following key codes
+	switch (keycode) {
+	case KC_ESC:
+	case KC_SPC:
+	case KC_ENT:
+	case KC_TAB:
+	case KC_DOT:
+	case KC_COMM:
+		if (record->event.pressed) { tap_code(KC_CAPS); }
+	}
+}
+#endif
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+#ifdef CAPSWORD_ENABLE
+	// Monitor key codes to toggle caps lock
+	if (host_keyboard_led_state().caps_lock) { process_caps_word(keycode, record); }
+#endif
     switch (keycode) {
         case QWERTY:
             if (record->event.pressed) {
                 set_single_persistent_default_layer(_QWERTY);
-                // Set all home row mod definitions
             }
             return false;
             break;
         case COLEMAK:
             if (record->event.pressed) {
                 set_single_persistent_default_layer(_COLEMAK);
-                // Set all home row mod definitions
             }
             return false;
             break;
     }
-    return true;
+    if (host_keyboard_led_state().caps_lock) { process_caps_word(keycode, record); }
+    return false;
 };
+
+#ifdef ENCODER_ENABLE
 // Rotary encoder to be added later
-// void encoder_update_user(uint8_t index, bool clockwise) {
-// if (IS_LAYER_ON(_QWERTY)) {
-//         if (clockwise) {
-//             register_code(KC_VOLU);
-//             unregister_code(KC_VOLU);
-//         } else {
-//             register_code(KC_VOLD);
-//             unregister_code(KC_VOLD);
-//         }
-//     } else {
-//         if (clockwise) {
-//             register_code(KC_VOLU);
-//             unregister_code(KC_VOLU);
-//         } else {
-//             register_code(KC_VOLD);
-//             unregister_code(KC_VOLD);
-//         }
-//     }
-// }
+void encoder_update_user(uint8_t index, bool clockwise) {
+if (IS_LAYER_ON(_QWERTY) || IS_LAYER_ON(_COLEMAK)) {
+        if (clockwise) {
+            register_code(KC_VOLU);
+            unregister_code(KC_VOLU);
+        } else {
+            register_code(KC_VOLD);
+            unregister_code(KC_VOLD);
+        }
+    } else {
+        if (clockwise) {
+            register_code(KC_VOLU);
+            unregister_code(KC_VOLU);
+        } else {
+            register_code(KC_VOLD);
+            unregister_code(KC_VOLD);
+        }
+    }
+}
+#endif
